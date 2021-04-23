44938b9deece82343b2198b2dca84d8c4833598e
malfiire <jaksontl12@gmail.com>
2021-04-23 07:41:19 +0200

    prueba

---
+++ src/app/instrumento/instrumento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instrumento',
+  templateUrl: './instrumento.component.html',
+  styleUrls: ['./instrumento.component.css']
+})
+export class InstrumentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/instrumento/instrumento.ts
@@ -0,0 +1,8 @@
+export class Instrumento{
+    id:number;
+    nombreInstrumento: string;
+    tipoInstrumento:string;
+    marcaInstrumento:string;
+    precioInstrumento:number;
+    cantidad:number;
+}
